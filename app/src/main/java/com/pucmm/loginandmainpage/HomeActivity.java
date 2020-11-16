0ae3d3e8c970697c01e2e46e6c7085e0adf6acf7
Pgrm97 <Pgrm97@gmail.com>
2020-11-15 23:15:43 -0400

    Drawer logs out successfully

--- app/src/main/java/com/pucmm/loginandmainpage/HomeActivity.java
+++ app/src/main/java/com/pucmm/loginandmainpage/HomeActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -40,7 +43,18 @@ public class HomeActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(HomeActivity.this, DrawerActivity.class));
+                CharSequence text = "Signing in..";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(HomeActivity.this, text, duration);
+                toast.show();
+                new Handler().postDelayed(new Runnable(){
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(HomeActivity.this, DrawerActivity.class));
+                    }
+                }, 2000);
+
             }
         });
 
