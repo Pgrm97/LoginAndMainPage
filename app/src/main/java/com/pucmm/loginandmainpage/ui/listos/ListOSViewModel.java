6ed00737286592197aa4962c772c4fa515d49a18
Pgrm97 <Pgrm97@gmail.com>
2020-11-15 22:31:56 -0400

    Created the ListOS Fragment

---
+++ app/src/main/java/com/pucmm/loginandmainpage/ui/listos/ListOSViewModel.java
@@ -0,0 +1,18 @@
+package com.pucmm.loginandmainpage;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ListOSViewModel extends ViewModel {
+    private MutableLiveData<String> mText;
+
+    public ListOSViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is the ListOS fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
