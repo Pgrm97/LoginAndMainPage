4c3abba91e866b9547ce8906e86d1b3b57b43863
Pgrm97 <Pgrm97@gmail.com>
2020-11-16 00:46:44 -0400

    Added the Storage Permission, finished HW.

--- app/src/main/java/com/pucmm/loginandmainpage/RegisterActivity2.java
+++ app/src/main/java/com/pucmm/loginandmainpage/RegisterActivity2.java
@@ -1,15 +1,28 @@
 package com.pucmm.loginandmainpage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity2 extends AppCompatActivity {
+
+    private int STORAGE_PERMISSION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,5 +55,76 @@ public class RegisterActivity2 extends AppCompatActivity {
                 startActivity(new Intent(RegisterActivity2.this, HomeActivity.class));
             }
         });
+
+        ImageView imageView = findViewById(R.id.imageView2);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(getApplicationContext(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(RegisterActivity2.this, "Opening File System", Toast.LENGTH_SHORT).show();
+                    Uri selected = Uri.parse(Environment.getExternalStorageState());
+                    Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                    intent.addCategory(Intent.CATEGORY_OPENABLE);
+                    intent.setType("image/*") ;
+                    intent.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    startActivity(intent);
+                }
+                else{
+                    requestStoragePermission(Manifest.permission.READ_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE);
+                }
+//                if (ContextCompat.checkSelfPermission(RegisterActivity2.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+//                {
+//                    Toast.makeText(RegisterActivity2.this, "Opening File System", Toast.LENGTH_SHORT).show();
+//                    Uri selected = Uri.parse(Environment.getExternalStorageState());
+//                    Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+//                    intent.addCategory(Intent.CATEGORY_OPENABLE);
+//                    intent.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+//                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+//                    startActivity(intent);
+//                }
+//                else{
+//                    Toast.makeText(RegisterActivity2.this, "Storage permission isn't granted", Toast.LENGTH_SHORT).show();
+//                }
+            }
+        });
+    }
+
+    public void requestStoragePermission(final String permission, final int CODE) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(RegisterActivity2.this, permission)){
+            new AlertDialog.Builder(RegisterActivity2.this)
+                    .setTitle("Permission needed")
+                    .setMessage("Permission is needed for app to work.")
+                    .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(RegisterActivity2.this, new String[]{permission}, CODE);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create()
+                    .show();
+        }
+        else{
+            ActivityCompat.requestPermissions(RegisterActivity2.this, new String[]{permission},CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == STORAGE_PERMISSION_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Storage Permission GRANTED", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(this, "Storage Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
