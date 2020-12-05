706bfaf6105903a9f61f78034a901e7940365ad9
Pgrm97 <Pgrm97@gmail.com>
2020-12-05 19:27:00 -0400

    Login works.

--- app/src/main/java/com/pucmm/loginandmainpage/HomeActivity.java
+++ app/src/main/java/com/pucmm/loginandmainpage/HomeActivity.java
@@ -9,7 +9,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,11 +20,15 @@ import com.pucmm.loginandmainpage.database.RoomDB;
 public class HomeActivity extends AppCompatActivity {
 
     private RoomDB database;
+    EditText emailText, passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //Initialize database
+        database = RoomDB.getInstance(HomeActivity.this);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
@@ -43,22 +49,50 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+
+        emailText = findViewById(R.id.editTextTextEmailAddress);
+        passwordText = findViewById(R.id.editTextTextPassword);
         Button loginButton = findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CharSequence text = "Signing in..";
-                int duration = Toast.LENGTH_SHORT;
+                InputMethodManager imm = (InputMethodManager)getSystemService(HomeActivity.this.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                String sEmail = emailText.getText().toString().trim();
+                String sPassword = passwordText.getText().toString().trim();
+                if(!sEmail.equals("") && !sPassword.equals("")){
+                    String password;
+                    password = database.userDao().getPasswordFromEmail(sEmail);
 
-                Toast toast = Toast.makeText(HomeActivity.this, text, duration);
-                toast.show();
-                new Handler().postDelayed(new Runnable(){
-                    @Override
-                    public void run() {
-                        startActivity(new Intent(HomeActivity.this, DrawerActivity.class));
+                    if(password == null){
+
+                        Toast.makeText(HomeActivity.this, "Password doesn't exist.", Toast.LENGTH_SHORT).show();
+                        emailText.setText("");
+                        passwordText.setText("");
                     }
-                }, 2000);
+                    else if (!password.equals("")) {
+                        if(password.equals(sPassword)){
+                            emailText.setText("");
+                            passwordText.setText("");
+                            CharSequence text = "Signing in..";
+                            int duration = Toast.LENGTH_SHORT;
 
+                            Toast toast = Toast.makeText(HomeActivity.this, text, duration);
+                            toast.show();
+                            new Handler().postDelayed(new Runnable(){
+                                @Override
+                                public void run() {
+                                    startActivity(new Intent(HomeActivity.this, DrawerActivity.class));
+                                }
+                            }, 2000);
+                        }
+                        else{
+                            Toast.makeText(HomeActivity.this, "Password doesn't match.", Toast.LENGTH_SHORT).show();
+                            emailText.setText("");
+                            passwordText.setText("");
+                        }
+                    }
+                }
             }
         });
 
